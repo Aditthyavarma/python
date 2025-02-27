9294deaaf91b96f3c451ab8dc19025038b655e30
Aditthyavarma <aditthyavarma007@gmail.com>
2025-02-27 21:45:36 +0530

    added star pattern modified.py

--- star_pattern.py
+++ star_pattern.py
@@ -1,3 +1,7 @@
 n = int(input("Enter the number of rows: "))
 for i in range(1, n + 1):
     print("* " * i)
+
+    n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    print("* " * i)
