13cec3e2c778170a481241151b627f9b7d3d4271
Aditthyavarma <aditthyavarma007@gmail.com>
2025-03-04 11:37:40 +0530

    Developed Guess The number game with a basic code

---
+++ guess_game.py
@@ -0,0 +1,14 @@
+import random
+
+number_to_guess = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess the number (1-100): "))
+    if guess < number_to_guess:
+        print("Too low!")
+    elif guess > number_to_guess:
+        print("Too high!")
+    else:
+        print("You got it!")
+        break
+
