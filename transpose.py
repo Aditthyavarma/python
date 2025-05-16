9cbcd5d375b15307c7767f8c85b25bd245168089
Aditthyavarma <aditthyavarma007@gmail.com>
2025-05-16 11:47:22 +0530

    Python Program to Find Transpose of a 3x3 Matrix

---
+++ transpose.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Original 3x3 matrix
+A = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+
+# Transpose of the matrix
+A_transpose = A.T
+
+# Print results
+print("Original Matrix:")
+print(A)
+
+print("\nTranspose of the Matrix:")
+print(A_transpose)
+
